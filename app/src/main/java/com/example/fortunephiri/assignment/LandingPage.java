48f06347cad928e71dea3146a0fff3e0b96953e0
Creig Fortune <fortunephiri004@gmail.com>
2018-11-10 23:35:38 -0500

    added listView with text and image. currently static text, not buttons.

--- app/src/main/java/com/example/fortunephiri/assignment/LandingPage.java
+++ app/src/main/java/com/example/fortunephiri/assignment/LandingPage.java
@@ -2,8 +2,13 @@ package com.example.fortunephiri.assignment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Adapter;
+
+import java.util.ArrayList;
 
 public class LandingPage extends AppCompatActivity {
+
+    private ArrayList <String> arrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
