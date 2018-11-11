a55e63fa251151832fd5a1dfdfeace8d9cc87af3
Creig Fortune <fortunephiri004@gmail.com>
2018-11-10 22:05:27 -0500

    correcting the functionality in login button. initially did not the startActivity() so action was never firing

--- app/src/main/java/com/example/fortunephiri/assignment/MainActivity.java
+++ app/src/main/java/com/example/fortunephiri/assignment/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(),LandingPage.class);
-
+                startActivity(intent);
             }
         });
     }
