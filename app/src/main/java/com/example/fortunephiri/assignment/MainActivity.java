0a814a240cf70569671c1b027417875a83ccaefc
Creig Fortune <fortunephiri004@gmail.com>
2018-11-10 21:58:36 -0500

    adding functionality to login button. to transfer to landing page.

--- app/src/main/java/com/example/fortunephiri/assignment/MainActivity.java
+++ app/src/main/java/com/example/fortunephiri/assignment/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.fortunephiri.assignment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btnLogin  =findViewById(R.id.btnLogin);
+            btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),LandingPage.class);
 
+            }
+        });
     }
 }
