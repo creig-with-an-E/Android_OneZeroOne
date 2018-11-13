e29b99c74475a7cf13b4466312754006331296cc
Creig Fortune <fortunephiri004@gmail.com>
2018-11-12 19:01:33 -0500

    adding twitter list item and icon, to list view. Adding MySchedule to project.

--- app/src/main/java/com/example/fortunephiri/assignment/LandingPage.java
+++ app/src/main/java/com/example/fortunephiri/assignment/LandingPage.java
@@ -11,18 +11,29 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 
 
 public class LandingPage extends AppCompatActivity {
 
-    private String [] textLink ={"My Schedule","Speakers","Maps: Places of interest","List of Attendees",
-                                    "Sponsors","Twitter"};
-    private int [] imageLink ={R.drawable.calendar,R.drawable.calendar,R.drawable.calendar,R.drawable.calendar,R.drawable.calendar,R.drawable.twitter};
+    private ArrayList<String> textLink = new ArrayList <String>() ;
+    private int [] imageLink ={R.drawable.calendar,R.drawable.calendar,R.drawable.calendar,R.drawable.calendar,R.drawable.calendar,R.drawable.calendar,R.drawable.twitter};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+        //creating the list
+        textLink.add("General Schedule");                //index:0 ->General Schedule
+        textLink.add("My Schedule");                     //index:1  ->My Schedule
+        textLink.add("Speakers");                       //index:2  ->Speakers
+        textLink.add("Maps: Places of interest");       //index:3  ->Maps
+        textLink.add("List of attendees");              //index:4  ->List attendees
+        textLink.add("Sponsors");                       //index:5  ->Sponsors
+        textLink.add("Twitter");                        //index:6  ->twitter
+
+
+
 
         ListView listView = findViewById(R.id.listview);
         CustomAdapter customAdapter = new CustomAdapter();
@@ -31,9 +42,16 @@ public class LandingPage extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                if(i==0){
-//                    Intent intent = new Intent(getApplicationContext(),NewActivity.class);
-//                    startActivity(intent);
+                switch(i){
+                    case 0:
+                        Intent intentGSchedule = new Intent(getApplicationContext(),MyScheduleActivity.class);
+                        startActivity(intentGSchedule); //handles General schedule intent
+                    case 1:
+                        Intent intentMySch = new Intent(getApplicationContext(),MyScheduleActivity.class);
+                        startActivity(intentMySch);     //starting My Schedule intent
+                        break;
+                    default:
+                        break;
                 }
             }
         });
@@ -57,12 +75,13 @@ public class LandingPage extends AppCompatActivity {
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
+            //fills the list(menu) with the ArrayList data
             View view1 = getLayoutInflater().inflate(R.layout.menu_data,null);
             //getting view in row_data
             TextView name = view1.findViewById(R.id.menuItem);
             ImageView image = view1.findViewById(R.id.imageView);
 
-            name.setText(textLink[i]);
+            name.setText(textLink.get(i));
             image.setImageResource(imageLink[i]);
 
             return view1;
