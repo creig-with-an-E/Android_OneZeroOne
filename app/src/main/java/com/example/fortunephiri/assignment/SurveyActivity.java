e048337227d81a73c49c7d278f47301b62998742
Creig Fortune <fortunephiri004@gmail.com>
2018-11-15 20:52:43 -0500

    minor refactor

--- app/src/main/java/com/example/fortunephiri/assignment/SurveyActivity.java
+++ app/src/main/java/com/example/fortunephiri/assignment/SurveyActivity.java
@@ -1,7 +1,12 @@
 package com.example.fortunephiri.assignment;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import java.io.OutputStreamWriter;
+import java.util.logging.Logger;
 
 public class SurveyActivity extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class SurveyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
+
+    findViewById(R.id.btnSurvey).setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String data ="this is data";
+            try{
+                OutputStreamWriter streamWriter = new OutputStreamWriter(getApplicationContext().openFileOutput("survey.txt", Context.MODE_PRIVATE));
+                streamWriter.write(data);
+                Log.d("Working","this was written");
+                streamWriter.close();
+            }catch (Exception e){
+                Log.d("Working","error occurred");
+                e.printStackTrace();
+            }
+
+        }
+    });
     }
 }
