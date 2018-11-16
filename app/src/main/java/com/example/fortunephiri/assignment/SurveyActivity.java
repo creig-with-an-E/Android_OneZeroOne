2e77c97101644214dad0d0bd83a2431ed74eb27b
Creig Fortune <fortunephiri004@gmail.com>
2018-11-15 22:02:13 -0500

    adding functionality to save method in the SurveyActivity. The method is called directly from the xml, as opposed to the regular onClick event listener used through out the application. The result is saved to survey.txt file separated by new lines.

--- app/src/main/java/com/example/fortunephiri/assignment/SurveyActivity.java
+++ app/src/main/java/com/example/fortunephiri/assignment/SurveyActivity.java
@@ -1,35 +1,55 @@
 package com.example.fortunephiri.assignment;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import java.io.OutputStreamWriter;
-import java.util.logging.Logger;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 
 public class SurveyActivity extends AppCompatActivity {
-
+    EditText q1, q2,q3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
 
-    findViewById(R.id.btnSurvey).setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            String data ="this is data";
-            try{
-                OutputStreamWriter streamWriter = new OutputStreamWriter(getApplicationContext().openFileOutput("survey.txt", Context.MODE_PRIVATE));
-                streamWriter.write(data);
-                Log.d("Working","this was written");
-                streamWriter.close();
-            }catch (Exception e){
-                Log.d("Working","error occurred");
+        q1 = findViewById(R.id.question1);
+        q2 = findViewById(R.id.question2);
+        q3 = findViewById(R.id.question3);
+
+    }
+
+    public void save(View view) {
+        String data =  q1.getText().toString() + "\n";
+               data += q3.getText().toString() + "\n";
+               data += q2.getText().toString() + "\n";
+
+        final String FILE_NAME = "survey.txt";
+        FileOutputStream fos =null;
+        try {
+            fos = openFileOutput(FILE_NAME,MODE_PRIVATE);
+            fos.write(data.getBytes());
+            q1.getText().clear();
+            q2.getText().clear();
+            q3.getText().clear();
+
+            Toast.makeText(this,"survey submitted",Toast.LENGTH_LONG).show();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-
         }
-    });
     }
 }
