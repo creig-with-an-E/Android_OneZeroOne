cf618f2c92831be9580b95edd86e388e73fc2886
Creig Fortune <fortunephiri004@gmail.com>
2018-11-29 15:19:06 -0500

    update to speakerActivity.java adding listview functionality which is passed the data by the new_data.xml

--- app/src/main/java/com/example/fortunephiri/assignment/DatabaseHelper.java
+++ app/src/main/java/com/example/fortunephiri/assignment/DatabaseHelper.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DatabaseHelper extends SQLiteOpenHelper {
@@ -12,7 +16,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     //database cols
     private static final String id ="idCol";
     private static final String firstnameCol ="firstname";
-    private static final String lastnameCol ="lastnameCol";
+    private static final String lastnameCol ="lastname";
     private static final String tableName ="Speakers";
     private static final String affiliationCol = "Affiliation";
     private static final String email ="email";
@@ -20,14 +24,23 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public DatabaseHelper(Context context){
         super(context, tableName,null,1);
+        Log.d("mina","database constructor");
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        Log.d("mina","onCreate database method call");
         String createTable ="Create table if not exists " + tableName + " (id Integer primary key autoincrement ," +
                 firstnameCol + " Text ," + lastnameCol + "Text ," + affiliationCol + " Text ," +
                 email + " Text, " + bio + " Text )";
+        dropTable();
         db.execSQL(createTable);
+        db.execSQL(String.format("insert into %s values('Sam','Smith','UNICEF','email@gmail.com','lead practitioner')", tableName));
+        db.execSQL(String.format("insert into %s values('Lisa','Vio','UNICEF','eml@gmail.com','practitioner')", tableName));
+        db.execSQL(String.format("insert into %s values('Creig','Phiri','Canker Corp','fortunephiri@gmail.com','Entreprenuer')", tableName));
+        db.execSQL(String.format("insert into %s values('Omie','Mdlo','Diaman Corp','email@gmail.com','Gemologist')", tableName));
+        db.execSQL(String.format("insert into %s values('Some','One','UNICEF','email@gmail.com','lead practitioner')", tableName));
+        db.execSQL(String.format("insert into %s values('John','Doe','UNICEF','email@gmail.com','lead practitioner')", tableName));
     }
 
     @Override
@@ -40,7 +53,17 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         String query = "Select * from " + tableName;
         Cursor data = db.rawQuery(query,null);
-        return data;
+        Log.d("data",data.toString());
 
+        return data;
     }
+
+    public void dropTable(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("drop table if exists " + tableName);
+        Log.i("DropTable","Table was dropped ekse");
+    }
+
+
+
 }
--- app/src/main/java/com/example/fortunephiri/assignment/SpeakersActivity.java
+++ app/src/main/java/com/example/fortunephiri/assignment/SpeakersActivity.java
@@ -1,15 +1,62 @@
 package com.example.fortunephiri.assignment;
-
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class SpeakersActivity extends AppCompatActivity {
-    DatabaseHelper databaseHelper;
+    ListView listview;
+    String [] firstnames ={"fortune", "creig"};
+    String [] lastnames = {"phiri" ,"malinga"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speakers);
+        listview = findViewById(R.id.SpeakerlistView);
 
-        databaseHelper.onCreate();
+        CustomAdapter customAdapter = new CustomAdapter();
+            listview.setAdapter(customAdapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Intent intent =new Intent(getApplicationContext(),ListActivityActivity.class);
+//                intent.putExtra("first",firstnames[position]);
+//                intent.putExtra("last",lastnames[position]);
+//                startActivity(intent);
+            }
+        });
+    }
+
+    private class CustomAdapter extends BaseAdapter{
+        @Override
+        public int getCount() {
+            return firstnames.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view =getLayoutInflater().inflate(R.layout.new_data, null);
+            TextView name =view.findViewById(R.id.firstname);
+            TextView last = view.findViewById(R.id.lastname);
+            name.setText(firstnames[position]);
+            last.setText(lastnames[position]);
+            return view;
+        }
     }
 }
